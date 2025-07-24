a179ed647a2293db5f87f5ba30c3b8f2be50d4d8
Ray-Garraty <vl.besed@gmail.com>
2025-07-24 07:21:14 +0300

    Added ESLint and Bootstrap

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const { app, BrowserWindow } = require('electron')
+import { app, BrowserWindow } from 'electron';
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600
-  })
-
-  win.loadFile('index.html')
+});
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
